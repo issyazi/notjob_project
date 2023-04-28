dbae12abe69e058d1cb221f1c4e5689a1d3305fc
Luti0 <103309167+Luti0@users.noreply.github.com>
2023-04-28 16:06:06 +1000

    Add files via upload
    Валидация js

---
+++ js/validation_reg.js
@@ -0,0 +1,108 @@
+const form = document.getElementById('form');
+const login = document.getElementById('login');
+const name1 = document.getElementById('name');
+const surname = document.getElementById('surname');
+const birthday = document.getElementById('birthday');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+const validateInputs = () => {
+
+    const loginValue = login.value.trim();  
+    const nameValue = name1.value.trim();
+    const surnameValue = surname.value.trim();
+    const birthdayValue = birthday.value;
+
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+
+    if(loginValue === '') {
+        setError(login, 'Login is required');
+    } else {
+        setSuccess(login);
+    }
+
+    if(birthdayValue === "") {
+        setError(birthday, 'Date is required');
+    } else {
+        setSuccess(birthday);
+    }
+    
+    if(nameValue === '') {
+        setError(name1, 'Name is required');
+    } else {
+        setSuccess(name1);
+    }
+    
+    if(surnameValue === '') {
+        setError(surname, 'Surname is required');
+    } else {
+        setSuccess(surname);
+    }
+
+    if(usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8 ) {
+        setError(password, 'Password must be at least 8 character.')
+    } else {
+        setSuccess(password);
+    }
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
