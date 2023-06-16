9c0fd08b7a373598706eb855e332fc0695453419
issyazi <91420149+issyazi@users.noreply.github.com>
2023-06-17 02:00:33 +1000

    Add files via upload

---
+++ js/height_comment_block.js
@@ -0,0 +1,5 @@
+var h1 = document.getElementById('height').offsetHeight;
+
+var height2 = document.getElementById('comments').offsetHeight;
+
+height2.style.setProperty('--comments-height', height1);
